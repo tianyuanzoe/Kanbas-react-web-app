77e2c80be6113612101b1bd2b69a7d827650a68c
tianyuanzoe <tian.yuan3@northeastern.edu>
2023-12-04 21:49:54 -0800

    change env variable

--- src/Project/Users/client.js
+++ src/Project/Users/client.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-export const BASE_API = process.env.REACT_APP_BASE_API_URL;
+export const BASE_API = process.env.REACT_APP_API_BASE || "http://localhost:4000";
 export const USERS_API = `${BASE_API}/api/users`;
 const request = axios.create({
   withCredentials: true,
